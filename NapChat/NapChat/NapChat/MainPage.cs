09b3b1cbdacd9849177ad2eb28c1ba785075a96e
Brandon Best <brandonbest94@live.ca>
2017-04-28 18:21:57 -0600

    Added main page and xml

---
+++ NapChat/NapChat/NapChat/MainPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace NapChat
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
