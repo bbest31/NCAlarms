ca6b0518ca2135fbb00e7774757e8bd3c7d6dcbb
Brandon Best <brandonbest94@live.ca>
2017-05-23 22:25:39 -0600

    View model for home page

---
+++ NapChat/NapChat/NapChat/ViewModel/HomePageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using NapChat.Services;
+using NapChat.Abstractions;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace NapChat.ViewModel
+{
+    public class HomePageViewModel : BaseViewModel
+    {
+        ICloudService cloudService;
+
+        public Command LogoutCommand { get; }
+
+        public HomePageViewModel()
+        {
+            Debug.WriteLine("In TaskListViewMOdel");
+         
+
+        }
+
+
+
+    }
+}
