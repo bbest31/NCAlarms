f8c5a02f7a30722b8cba8a931d2db273a71e8c2a
Willy Liao <willyliao0425@gmail.com>
2017-05-27 13:41:55 -0700

    Take Over best

---
+++ NapChat/NapChat/NapChat/Helpers/NapChatSingletons.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NapChat.Abstractions;
+using NapChat.Services;
+
+namespace NapChat.Helpers
+{
+    public static class NapChatSingletons
+    {
+        public static ICloudService CloudService { get; set; }
+
+        static NapChatSingletons()
+        {
+            CloudService = new AzureCloudService();
+
+        }
+
+    }
+}
