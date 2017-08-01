b12065ecc16932f4039e32eb52f6baa41eeab28f
Brandon Best <brandonbest94@live.ca>
2017-08-01 16:05:57 -0600

    New ALarmView PAge

---
+++ NapChat/NapChat/NapChat/Pages/AlarmView.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace NapChat.Pages
+{
+	public class AlarmView : ContentPage
+	{
+        #region Views
+        Label timeDisplayLabel;
+        Button snoozeBtn;
+        Button dismissBtn;
+        StackLayout alarmViewStackLayout;
+        #endregion
+
+        String TimeDisplayText = "HI";
+
+		public AlarmView ()
+		{
+            timeDisplayLabel = new Label()
+            {
+                Text = TimeDisplayText,
+                HorizontalOptions = LayoutOptions.Center,
+                TextColor = Color.Aquamarine,
+                FontSize = 55,
+            };
+
+            alarmViewStackLayout = new StackLayout()
+            {
+                Children = {
+                    timeDisplayLabel,
+                }
+            };
+            Content = alarmViewStackLayout;
+			
+		}
+	}
+}
