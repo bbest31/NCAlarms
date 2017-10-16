d56e35ac89ea1560035c72dfb2a5b9ed33545438
bbest31 <brandonbest94@live.ca>
2017-10-16 11:46:21 -0600

    Made sign up activity with new user sign up method and sign up button on login activity.

--- NapChatAlarmsAndroid/app/src/main/java/com/napchatalarms/napchatalarmsandroid/LoginActivity.java
+++ NapChatAlarmsAndroid/app/src/main/java/com/napchatalarms/napchatalarmsandroid/LoginActivity.java
@@ -1,8 +1,17 @@
 package com.napchatalarms.napchatalarmsandroid;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -10,11 +19,52 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
+    private String email;
+    private String password;
+
+    /**Button used to navigate to the SignUpActivity.**/
+    Button signUpButton;
+
+    public void initializeViews(){
+        signUpButton = (Button) findViewById(R.id.signUp_btn);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_login);
+
+        initializeViews();
+
+        signUpButton.setOnClickListener(new View.OnClickListener(){
+        @Override
+            public void onClick(View v){
+            Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
+            startActivity(intent);
+        }
+
+        });
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                           // Log.d( "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            loginNavigationOnSuccess(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w("signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            loginNavigationOnSuccess(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
 
     @Override
@@ -28,15 +78,20 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+
+
     /**
-     * This method is called if the user is succesfully signed in and will navigate
+     * This method is called if the user is successfully signed in and will navigate
      * to the Home Activity by confirming the FirebaseUser object for the current user
      * is a non-null object.**/
     public void loginNavigationOnSuccess(FirebaseUser currentUser){
 
         if(currentUser != null){
-            
+            //Intent homeIntent = new Intent(LoginActivity.this, HomeActivity.class);
+            //startActivity(homeIntent);
         }
 
     }
+
+
 }
--- NapChatAlarmsAndroid/app/src/main/java/com/napchatalarms/napchatalarmsandroid/SignUpActivity.java
+++ NapChatAlarmsAndroid/app/src/main/java/com/napchatalarms/napchatalarmsandroid/SignUpActivity.java
@@ -1,13 +1,66 @@
 package com.napchatalarms.napchatalarmsandroid;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
     }
+
+
+    /**
+     * This method takes in the new users email, password, surname and first name in order to create
+     * a new account for them and log them in using thier entered credentials.
+     * **/
+    public void signUpNewUser(String email, String password, String surname, String firstName){
+
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            signUpNavigationOnSuccess(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                           // Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            signUpNavigationOnSuccess(null);
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+    public void signUpNavigationOnSuccess(FirebaseUser currentUser){
+
+        if(currentUser != null){
+
+        }
+
+    }
+
 }
