db2585e11c5ded1a5fe5eb212342270860dbc1d8
PeterWeckend <pweckend@ualberta.ca>
2018-05-19 12:02:44 -0600

    Misc Options page changes

---
+++ NapChatAlarmsiOS/NapchatAlarms/NapchatAlarms/Views/OptionsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OptionsTableViewCell.swift
+//  NapchatAlarms
+//
+//  Created by Peter Weckend on 2018-05-16.
+//  Copyright © 2018 Napchat Alarms. All rights reserved.
+//
+
+import UIKit
+
+class OptionsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
