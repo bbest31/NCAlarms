89e67938256c9012e89de2e1a042d8bac4e5de0e
liaowilly12123 <willyliao0425@gmail.com>
2018-05-30 19:08:49 -0700

    Working on Google Sign in.

--- NapChatAlarmsiOS/NapchatAlarms/NapchatAlarms/Controllers/LandingPageViewController.swift
+++ NapChatAlarmsiOS/NapchatAlarms/NapchatAlarms/Controllers/LandingPageViewController.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
-class LandingPageViewController: UIViewController {
+class LandingPageViewController: UIViewController, GIDSignInUIDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        checkUserLoggedIn()
 
-        // Do any additional setup after loading the view.
+        
+        // TODO(developer) Configure the sign-in button look/feel
+        // ...
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,6 +45,52 @@ class LandingPageViewController: UIViewController {
     
     @IBAction func unwindToLandingPageWithSegue(segue:UIStoryboardSegue) {
         
+    }
+    
+    @IBOutlet weak var GoogleSignInButton: GIDSignInButton!
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+        // ...
+        print("1")
+        if let error = error {
+            // ...
+            print("2")
+
+            return
+        }
+        print("3")
+
+        guard let authentication = user.authentication else { return }
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+                                                       accessToken: authentication.accessToken)
+        // ...
+        Auth.auth().signInAndRetrieveData(with: credential) { (authResult, error) in
+            if let error = error {
+                // ...
+                print("4")
+
+                return
+            }
+            // User is signed in
+            // ...
+            print("signedin")
+            self.performSegue(withIdentifier: "segueLandingToHome", sender: self)
+            
+        }
+    }
+    
+    func checkUserLoggedIn() {
+        if Auth.auth().currentUser?.uid != nil {
+            //user is logged in
+            print("logged in")
+            self.performSegue(withIdentifier: "segueLandingToHome", sender: self)
+        
+        }else{
+            //user is not logged in
+            print("not logged in")
+            GIDSignIn.sharedInstance().uiDelegate = self
+            GIDSignIn.sharedInstance().signIn()
+        }
     }
     
 
