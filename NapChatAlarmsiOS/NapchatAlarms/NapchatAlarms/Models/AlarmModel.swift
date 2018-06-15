c3b77d8ad7376d63d0d3d0165f41c82e66d669c9
PeterWeckend <pweckend@ualberta.ca>
2018-06-14 21:10:44 -0600

    Modified the AlarmModel class so it can be locally saved to the user's device

--- NapChatAlarmsiOS/NapchatAlarms/NapchatAlarms/Models/AlarmModel.swift
+++ NapChatAlarmsiOS/NapchatAlarms/NapchatAlarms/Models/AlarmModel.swift
@@ -7,17 +7,67 @@
 //
 
 import UIKit
+import os.log
 
-class AlarmModel {
+class AlarmModel: NSObject, NSCoding {
+    
+    // MARK: Properties
+    struct PropertyKey {
+        static let timeString = "timeString"
+        static let time = "time"
+        static let repeatInd = "repeatInd"
+        static let alarmSoundInd = "alarmSoundInd"
+        static let isEnabled = "isEnabled"
+    }
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("alarms")
+    
     var timeString: String // temporary until a proper timestamp is used
     var time: Date // in UTC
     var repeatInd: String?
     var alarmSoundInd: String?
     var isEnabled: Bool
     
-    init(time: Date, timeString: String, isEnabled: Bool) {
+    init(time: Date, timeString: String, repeatInd: String? = nil, alarmSoundInd: String? = nil, isEnabled: Bool) {
         self.time = time
         self.timeString = timeString
+        self.repeatInd = nil
+        self.alarmSoundInd = nil
         self.isEnabled = isEnabled
     }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(timeString, forKey: PropertyKey.timeString)
+        aCoder.encode(time, forKey: PropertyKey.time)
+        aCoder.encode(repeatInd, forKey: PropertyKey.repeatInd)
+        aCoder.encode(alarmSoundInd, forKey: PropertyKey.alarmSoundInd)
+        aCoder.encode(isEnabled, forKey: PropertyKey.isEnabled)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        guard let timeString = aDecoder.decodeObject(forKey: PropertyKey.timeString) as? String else {
+            os_log("Unable to decode the timeString for an AlarmModel object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        guard let time = aDecoder.decodeObject(forKey: PropertyKey.time) as? Date else {
+            os_log("Unable to decode the time for an AlarmModel object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        let repeatInd = aDecoder.decodeObject(forKey: PropertyKey.repeatInd) as? String
+        
+        let alarmSoundInd = aDecoder.decodeObject(forKey: PropertyKey.alarmSoundInd) as? String
+        
+        guard let isEnabled = aDecoder.decodeObject(forKey: PropertyKey.isEnabled) as? Bool else {
+            os_log("Unable to decode the isEnabled property for an AlarmModel object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        
+        self.init(time: time, timeString: timeString, repeatInd: repeatInd, alarmSoundInd: alarmSoundInd, isEnabled: isEnabled)
+    }
 }
+
+
